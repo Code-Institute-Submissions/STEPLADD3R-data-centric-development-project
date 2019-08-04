ea7a2bfed50f3ff9ba5075b797fcc7a34b31e248
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2019-08-04 16:40:07 +0000

    hooked up MongoDB and tested the connection works

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 # Create an instance of a Flask app
 app = Flask(__name__)
 
+# Mongo DB Setup
+app.config['MONGO_DBNAME'] = 'book_reviewer'
+app.config['MONGO_URI'] = 'mongodb+srv://root:48Kcr5un1Mmlwpw5@datacentricdevelopment-hxsk1.mongodb.net/book_reviewer?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def test():
-    return 'Test'
+@app.route('/books')
+def get_books():
+    return render_template('books.html', books=mongo.db.books.find())
 
 
 # Set up IP address and Port number
