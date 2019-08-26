fb796a7b14f329c1d3a6b29fa8762624c7ea7439
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2019-08-26 21:24:59 +0000

    pep8-ifying app.py

--- app.py
+++ app.py
@@ -20,8 +20,12 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 mongo = PyMongo(app)
 
+
 @app.context_processor
 def global_template_variables():
+    """
+        This is used to pass in global variables to the base template
+    """
     navbar_genres = mongo.db.genres.find().limit(4)
     footer_genres = mongo.db.genres.find()
     footer_top_picks = mongo.db.books.find({ 'top_pick' : 'on' }).limit(4)
@@ -30,31 +34,40 @@ def global_template_variables():
     return dict(navbar_genres=navbar_genres, footer_genres=footer_genres, footer_top_picks=footer_top_picks, footer_user_picks=footer_user_picks, footer_recent_books=footer_recent_books)
 
 
-# File Upload Helper Functions
-# Checks if the extension is allowed
 def allowed_file(filename):
+    """
+        Helper function
+        Check if the extension is allowed
+    """
     return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
-# Retrieve the URL of the upload from MongoDB
 @app.route('/uploads/<filename>')
 def upload(filename):
+    """
+        Helper function
+        Retrieve the URL of the upload from MongoDB
+    """
     return mongo.send_file(filename)
 
 
-# Get the search term
 @app.route('/search/', methods=['POST'])
 @app.route('/books/search/', methods=['POST'])
 def search_books():
+    """
+        Get the search term
+    """
     search_term = request.form.get('search_term')
     return redirect(url_for('search_results', search_term=search_term))
 
 
-# Return the search results
 @app.route('/search/<search_term>')
 @app.route('/books/search/<search_term>')
 def search_results(search_term):
+    """
+        Return the search results
+    """
     mongo.db.books.create_index([( 'name', 'text' )])
     search_results = mongo.db.books.find({ '$text': { '$search' : search_term } }).limit(8)
     search_results_count = mongo.db.books.find({ '$text': { '$search' : search_term } }).count()
@@ -64,9 +77,11 @@ def search_results(search_term):
 #
 #   BOOKS
 #
-# Return top picks and recent books
 @app.route('/')
 def index():
+    """
+        Return top picks and recent books
+    """
     top_picks = mongo.db.books.find({ 'top_pick' : 'on' }).sort([( '$natural', -1 )]).limit(8)
     recent_books = mongo.db.books.find().sort([( '$natural', -1 )]).limit(8)
     return render_template('index.html', top_picks=top_picks, recent_books=recent_books)
@@ -74,20 +89,29 @@ def index():
 
 @app.route('/books')
 def get_books():
+    """
+        Return a list of all the books
+    """
     books = mongo.db.books.find()
     return render_template('books.html', books=books)
 
 
-# Create Book (Form)
 @app.route('/book/add')
 def create_book():
+    """
+        Create Book (Form)
+    """
     genres = mongo.db.genres.find()
     return render_template('create-book.html', genres=genres)
 
 
-# Insert into the DB
 @app.route('/book/insert', methods=['POST'])
 def insert_book():
+    """
+        Insert into the DB
+        Has some validation, but some validation is also handled
+        by Bootstrap, ensuring fields must be populated
+    """
     if 'cover_photo' not in request.files:
         flash('The uploaded file MUST have a file name.', 'error')
         return redirect(url_for('create_book'))
@@ -121,9 +145,11 @@ def insert_book():
         return redirect(url_for('create_book'))
 
 
-# Read Book
 @app.route('/book/<book_id>')
 def read_book(book_id):
+    """
+        Read Book / Reviews
+    """
     the_book = mongo.db.books.find_one({ '_id' : ObjectId(book_id) })
     genres = mongo.db.genres.find()
     reviews = list(mongo.db.reviews.find({ 'book_id' : book_id }))
@@ -135,16 +161,21 @@ def read_book(book_id):
     return render_template('read-book.html', book=the_book, genres=genres, reviews=reviews, average_rating=average_rating)
 
 
-# Update Book (Form)
 @app.route('/book/<book_id>/edit')
 def edit_book(book_id):
+    """
+        Update Book (Form)
+    """
     the_book = mongo.db.books.find_one({ '_id' : ObjectId(book_id) })
     return render_template('edit-book.html', book=the_book, genres=mongo.db.genres.find())
 
 
-# Update the DB
 @app.route('/book/<book_id>/update', methods=['POST'])
 def update_book(book_id):
+    """
+        Update the DB
+        Again, has some validation here, and some handled by Bootstrap
+    """
     if 'cover_photo' in request.files:
         cover_photo = request.files['cover_photo']
 
@@ -197,9 +228,11 @@ def update_book(book_id):
                 return redirect(url_for('edit_book', book_id=ObjectId(book_id)))
 
 
-# Delete Book
 @app.route('/book/<book_id>/delete')
 def delete_book(book_id):
+    """
+        Delete Book
+    """
     mongo.db.books.remove({'_id' : ObjectId(book_id)})
     flash('Delete Successful.', 'success')
     return redirect(url_for('get_books'))
@@ -208,22 +241,28 @@ def delete_book(book_id):
 #
 #   GENRES
 #
-# Returns a list of genres
 @app.route('/genres')
 def get_genres():
+    """
+        Returns a list of genres
+    """
     genres = mongo.db.genres.find()
     return render_template('genres.html', genres=genres)
 
 
-# Create Genre (Form)
 @app.route('/genre/add')
 def create_genre():
+    """
+        Create Genre (Form)
+    """
     return render_template('create-genre.html')
 
 
-# Insert into the DB
 @app.route('/genre/insert', methods=['POST'])
 def insert_genre():
+    """
+        Insert into the DB
+    """
     genres = mongo.db.genres
     genres.insert_one({
         'genre' : request.form.get('genre').lower(),
@@ -232,27 +271,32 @@ def insert_genre():
     return redirect(url_for('get_genres'))
 
 
-# Read Genre (from Books)
 @app.route('/genre/<genre>')
 def read_genre(genre):
+    """
+        Read Genre (from Books)
+    """
     the_genre = genre
     the_genre_id = mongo.db.genres.find_one({ 'genre' : genre })
     books = mongo.db.books.find({ 'genres' : genre })
     return render_template('read-genre.html', the_genre=the_genre, the_genre_id=the_genre_id, books=books)
 
 
-# Update Genre (Form)
 @app.route('/genre/<genre_id>/edit')
 def edit_genre(genre_id):
+    """
+        Update Genre (Form)
+    """
     the_genre = mongo.db.genres.find_one({ '_id' : ObjectId(genre_id) })
     return render_template('edit-genre.html', genre=the_genre)
 
 
-# Update the DB
 @app.route('/genre/<genre_id>/update', methods=['POST'])
 def update_genre(genre_id):
+    """
+        Update the DB
+    """
     genres = mongo.db.genres
-    
     genres.update_one(
         {'_id' : ObjectId(genre_id)},
         {
@@ -265,9 +309,11 @@ def update_genre(genre_id):
     return redirect(url_for('get_genres'))
 
 
-# Delete Genre
 @app.route('/genre/<genre_id>/delete')
 def delete_genre(genre_id):
+    """
+        Delete Genre
+    """
     mongo.db.genres.remove({'_id' : ObjectId(genre_id)})
     flash('Delete Successful.', 'success')
     return redirect(url_for('get_genres'))
@@ -276,9 +322,11 @@ def delete_genre(genre_id):
 #
 #   REVIEWS
 #
-# Insert into the DB
 @app.route('/book/<book_id>/review/insert', methods=['POST'])
 def insert_review(book_id):
+    """
+        Insert into the DB to be viewed via read_book()
+    """
     reviews = mongo.db.reviews
     reviews.insert_one({
         'book_id' : request.form.get('book_id'),
