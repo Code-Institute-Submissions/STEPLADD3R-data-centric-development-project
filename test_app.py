689d1e1d91f8c8e97cc9d3f7aa24dd2532a7e5a0
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2019-08-27 02:30:25 +0000

    tested empty/not empty search result

--- test_app.py
+++ test_app.py
@@ -1,8 +1,9 @@
 import app
 
-def test_search_results():
+def test_search_results_empty():
     """
         Test Search Functionality
+        Test a random string that should be empty
     """
     search_term = 'AsdSSSSdasdadf123213'
     search_results_count = app.mongo.db.books.count_documents({'$text': {'$search': search_term}})
@@ -11,3 +12,17 @@ def test_search_results():
     else:
         search_results = app.mongo.db.books.find({'$text': {'$search': search_term}})
     assert search_results == 'Empty'
+
+
+def test_search_results_not_empty():
+    """
+        Test Search Functionality
+        Test an actual book inside the MongoDB
+    """
+    search_term = 'A Brightness Long Ago'
+    search_results_count = app.mongo.db.books.count_documents({'$text': {'$search': search_term}})
+    if search_results_count < 1:
+        search_results = 'Empty'
+    else:
+        search_results = app.mongo.db.books.find({'$text': {'$search': search_term}})
+    assert search_results != 'Empty'
