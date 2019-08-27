fe026922b23e20ff3c1f3adb0d13615c291feeb8
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2019-08-27 02:12:02 +0000

    added first pytest

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import app
+
+def test_search_results():
+    """
+        Test Search Functionality
+    """
+    search_term = 'AsdSSSSdasdadf123213'
+    db_index = app.mongo.db.books.create_index([('name', 'text')])
+    search_results_count = app.mongo.db.books.count_documents({'$text': {'$search': search_term}})
+    if search_results_count < 1:
+        search_results = 'Empty'
+    else:
+        search_results = app.mongo.db.books.find({'$text': {'$search': search_term}})
+    assert search_results
