7290b59d6e771901d40b451208afb06c4bf9fd44
Ubuntu <ubuntu@ip-172-31-80-169.ec2.internal>
2019-08-27 02:18:12 +0000

    update to test_search_results in test_app.py

--- test_app.py
+++ test_app.py
@@ -5,10 +5,9 @@ def test_search_results():
         Test Search Functionality
     """
     search_term = 'AsdSSSSdasdadf123213'
-    db_index = app.mongo.db.books.create_index([('name', 'text')])
     search_results_count = app.mongo.db.books.count_documents({'$text': {'$search': search_term}})
     if search_results_count < 1:
         search_results = 'Empty'
     else:
         search_results = app.mongo.db.books.find({'$text': {'$search': search_term}})
-    assert search_results
+    assert search_results == 'Empty'
